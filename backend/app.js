bfa330add7e41345fe779cbae83733f0991b64a9
viji-spericorn <119667339+viji-spericorn@users.noreply.github.com>
2023-10-11 12:06:12 +0530

    console in app

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 var app = express();
 
 app.use(cors());
-
+console.log('hi');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
